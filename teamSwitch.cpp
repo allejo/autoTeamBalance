34dc49472b7bf682c7a61f0a9a2881fd5cf44d0f
Vladimir Jimenez <allejo@me.com>
2012-08-21 21:12:09 -0700

    Patched multiple bugs; renamed to Team Swap

--- teamSwitch.cpp
+++ teamSwitch.cpp
@@ -25,7 +25,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-*** kierra Details ***
+*** teamSwitch Details ***
 Author:
 Vladimir Jimenez (allejo)
 
@@ -49,10 +49,10 @@ Version:
 #include "../../src/bzfs/bzfs.h"
 #include "../../src/bzfs/CmdLineOptions.h"
 
-class kierra : public bz_Plugin, public bz_CustomSlashCommandHandler
+class teamSwitch : public bz_Plugin, public bz_CustomSlashCommandHandler
 {
 public:
-    virtual const char* Name (){return "kierra";}
+    virtual const char* Name (){return "Team Switch";}
     virtual void Init(const char* /*commandLine*/);
     virtual void Cleanup(void);
 
@@ -61,11 +61,12 @@ public:
     
     bool teamsUneven;
     double timeFirstUneven;
+    bz_eTeamType strongTeam;
 };
 
-BZ_PLUGIN(kierra);
+BZ_PLUGIN(teamSwitch);
 
-void kierra::Init(const char* /*commandLine*/)
+void teamSwitch::Init(const char* /*commandLine*/)
 {
     Register(bz_eCaptureEvent);
     Register(bz_eTickEvent);
@@ -74,18 +75,18 @@ void kierra::Init(const char* /*commandLine*/)
     
     teamsUneven = false;
 
-    bz_debugMessage(4,"kierra plugin loaded");
+    bz_debugMessage(4,"teamSwitch plugin loaded");
 }
 
-void kierra::Cleanup()
+void teamSwitch::Cleanup()
 {
     Flush();
     bz_removeCustomSlashCommand("switch");
 
-    bz_debugMessage(4,"kierra plugin unloaded");
+    bz_debugMessage(4,"teamSwitch plugin unloaded");
 }
 
-void kierra::Event(bz_EventData* eventData)
+void teamSwitch::Event(bz_EventData* eventData)
 {
     switch (eventData->eventType)
     {
@@ -94,16 +95,24 @@ void kierra::Event(bz_EventData* eventData)
             bz_CTFCaptureEventData_V1* ctfdata = (bz_CTFCaptureEventData_V1*)eventData;
             
             if (bz_getTeamCount(eRedTeam) == 0 || bz_getTeamCount(eBlueTeam) == 0) break;
-
-            if (bz_getTeamCount(eRedTeam)/bz_getTeamCount(eBlueTeam) < 1.3)
+            
+            if (bz_getTeamCount(eBlueTeam)/double(bz_getTeamCount(eRedTeam)) > 1.3 || bz_getTeamCount(eRedTeam)/double(bz_getTeamCount(eBlueTeam)) > 1.3)
             {
-                switchPlayer(ctfdata->playerCapping, "blue");
-                bz_sendTextMessage(BZ_SERVER, ctfdata->playerCapping, "You're an asshole for capping! You've been moved to the blue team.");
-            }
-            else if (bz_getTeamCount(eBlueTeam)/bz_getTeamCount(eRedTeam) < 1.3)
-            {
-                switchPlayer(ctfdata->playerCapping, "red");
-                bz_sendTextMessage(BZ_SERVER, ctfdata->playerCapping, "You're an asshole for capping! You've been moved to the red team.");
+                if (bz_getTeamCount(eRedTeam) > bz_getTeamCount(eBlueTeam)) strongTeam = eRedTeam;
+                else strongTeam = eBlueTeam;
+                
+                if (strongTeam == eRedTeam)
+                {
+                    switchPlayer(ctfdata->playerCapping, "blue");
+                    bz_killPlayer(ctfdata->playerCapping, 0);
+                    bz_sendTextMessage(BZ_SERVER, ctfdata->playerCapping, "You're an asshole for capping unfairly! You've been moved to the blue team.");
+                }
+                else
+                {
+                    switchPlayer(ctfdata->playerCapping, "red");
+                    bz_killPlayer(ctfdata->playerCapping, 0);
+                    bz_sendTextMessage(BZ_SERVER, ctfdata->playerCapping, "You're an asshole for capping unfairly! You've been moved to the red team.");
+                }
             }
         }
         break;
@@ -113,23 +122,40 @@ void kierra::Event(bz_EventData* eventData)
             bz_TickEventData_V1* tickdata = (bz_TickEventData_V1*)eventData;
             
             if (bz_getTeamCount(eRedTeam) == 0 || bz_getTeamCount(eBlueTeam) == 0) break;
-
-            if (bz_getTeamCount(eRedTeam)/bz_getTeamCount(eBlueTeam) < 1.3 && !teamsUneven)
+            
+            double redToBlueRatio = bz_getTeamCount(eRedTeam)/double(bz_getTeamCount(eBlueTeam));
+            double blueToRedRatio = bz_getTeamCount(eBlueTeam)/double(bz_getTeamCount(eRedTeam));
+            
+            if ((redToBlueRatio > 1.3 || blueToRedRatio > 1.3) && !teamsUneven)
             {
+                if (redToBlueRatio > 1.3) strongTeam = eRedTeam;
+                else strongTeam = eBlueTeam;
+                
                 teamsUneven = true;
                 timeFirstUneven = tickdata->eventTime;
+                bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, "Teams are uneven, please don't be dicks and even them out.");
             }
-            else if (bz_getTeamCount(eBlueTeam)/bz_getTeamCount(eRedTeam) < 1.3 && !teamsUneven)
+            else if (teamsUneven)
             {
-                teamsUneven = true;
-                timeFirstUneven = tickdata->eventTime;
-            }
-            else if ((bz_getTeamCount(eBlueTeam)/bz_getTeamCount(eRedTeam) < 1.3 || bz_getTeamCount(eRedTeam)/bz_getTeamCount(eBlueTeam) < 1.3) && teamsUneven)
-            {
-                teamsUneven = false;
+                if (strongTeam == eRedTeam)
+                {
+                    if (redToBlueRatio < 1.3)
+                    {
+                        teamsUneven = false;
+                        bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, "Thank you for evening out the teams.");
+                    }
+                }
+                else
+                {
+                    if (blueToRedRatio < 1.3)
+                    {
+                        teamsUneven = false;
+                        bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, "Thank you for evening out the teams.");
+                    }
+                }
             }
             
-            if (teamsUneven && (timeFirstUneven + 30 == bz_getCurrentTime()))
+            if (teamsUneven && (timeFirstUneven + 30 < bz_getCurrentTime()))
             {
                 bz_sendTextMessage(BZ_SERVER, BZ_ALLUSERS, "Balancing unfair teams...");
                 
@@ -144,24 +170,12 @@ void kierra::Event(bz_EventData* eventData)
                 int playerMoved = 0;
                 for (int i = 0; i < playersToMove; i++)
                 {
-                    while (true)
-                    {
-                        playerMoved = bz_randomPlayer();
-                        bz_BasePlayerRecord* pr = bz_getPlayerByIndex(playerMoved);
-                        
-                        if (pr->team != strongTeam)
-                        {
-                            bz_freePlayerRecord(pr);
-                            break;
-                        }
-                        
-                        bz_freePlayerRecord(pr);
-                    }
+                    playerMoved = bz_randomPlayer(strongTeam);
                     
                     if (strongTeam == eBlueTeam)
                     {
                         switchPlayer(playerMoved, "red");
-                        bz_sendTextMessage(BZ_SERVER, playerMoved, "You have been switched to the communist team");
+                        bz_sendTextMessage(BZ_SERVER, playerMoved, "You have been switched to the communist team.");
                     }
                     else if (strongTeam == eRedTeam)
                     {
@@ -169,6 +183,8 @@ void kierra::Event(bz_EventData* eventData)
                         bz_sendTextMessage(BZ_SERVER, playerMoved, "You have been switched to the blue team.");
                     }
                 }
+                
+                teamsUneven = false;
             }
         }
         break;
@@ -178,43 +194,23 @@ void kierra::Event(bz_EventData* eventData)
     }
 }
 
-bool kierra::SlashCommand(int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList *params)
+bool teamSwitch::SlashCommand(int playerID, bz_ApiString command, bz_ApiString message, bz_APIStringList *params)
 {
-    if (command == "switch")
+    if (command == "switch" && bz_hasPerm(playerID, "switch"))
     {
         if (params->size() != 1 && params->size() != 2)
         {
-            if (bz_hasPerm(playerID, "ban"))
-            {
-                bz_sendTextMessage(BZ_SERVER, playerID, "Syntax: /switch <player slot or callsign (optional)> rogue|red|green|blue|purple|observer");
-            }
-            else
-            {
-                bz_sendTextMessage(BZ_SERVER, playerID, "Syntax: /switch rogue|red|green|blue|purple|observer");
-            }
-            return true;
-        }    
-        else if (!bz_hasPerm(playerID, "ban") && params->size() > 1)
-        {
-            bz_sendTextMessage(BZ_SERVER, playerID, "Syntax: /switch rogue|red|green|blue|purple|observer");
+            bz_sendTextMessage(BZ_SERVER, playerID, "Syntax: /switch <player slot> rogue|red|green|blue|purple|observer");
             return true;
         }
         
         int myID;
         std::string teamToSwitchTo;
         
-        if (bz_hasPerm(playerID, "ban"))
+        if (params->size() == 2)
         {
-            if (params->size() == 2)
-            {
-                myID = atoi(std::string(params->get(0).c_str()).erase(0,1).c_str());
-                teamToSwitchTo = params->get(1).c_str();
-            }
-            else
-            {
-                myID = playerID;
-                teamToSwitchTo = params->get(0).c_str();
-            }
+            myID = atoi(std::string(params->get(0).c_str()).erase(0,1).c_str());
+            teamToSwitchTo = params->get(1).c_str();
         }
         else
         {
@@ -222,8 +218,19 @@ bool kierra::SlashCommand(int playerID, bz_ApiString command, bz_ApiString messa
             teamToSwitchTo = params->get(0).c_str();
         }
         
-        if (isValidPlayerID(myID) || isValidCallsign(params->get(0).c_str()))
+        if (isValidPlayerID(myID))
         {
+            bz_PlayerRecordV2* pr = (bz_PlayerRecordV2*)bz_getPlayerByIndex(myID);
+            
+            if (pr->team == eObservers && pr->motto == "bzadmin")
+            {
+                bz_sendTextMessage(BZ_SERVER, playerID, "Warning: In order to prevent bzadmin clients from crashing, you cannot 'switch' bzadmin clients.");
+                bz_freePlayerRecord(pr);
+                return true;
+            }
+            
+            bz_freePlayerRecord(pr);
+            
             if (strcmp(teamToSwitchTo.c_str(), "rogue") == 0 ||
                 strcmp(teamToSwitchTo.c_str(), "red")  == 0 ||
                 strcmp(teamToSwitchTo.c_str(), "blue")  == 0 ||
